f979ff467e6ee0163d5eb9718f7dcefa1dab0dab
ClAndEllen <1961075191@qq.com>
2021-01-24 12:43:36 +0800

    Initial commit

---
+++ app/src/main/java/com/ellen/androidktoptimization/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.ellen.androidktoptimization
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.ellen.androidktoptimization.ifop.Condition
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var a = 25
+        var b = 27
+        Condition()
+            .check(a > b, {
+                a = 28
+            }, {
+                Log.e("Ellen2021", "a<b")
+            })
+            .checkNoCallback(a > 26)
+            .checkFalseCallback(a < b) {
+                Log.e("Ellen2021", "a等于28时，a<b")
+            }.checkTrueCallback(a < b) {
+            Log.e("Ellen2021", "a等于28时，a>b")
+        }.start()
+
+        if(a>b){
+            a = 28
+            if(a>26){
+                if(a >= b){
+                    Log.e("Ellen2021", "a等于28时，a<b")
+                    if(a<b){
+                        Log.e("Ellen2021", "a等于28时，a>b")
+                    }
+                }
+            }
+        }else{
+            Log.e("Ellen2021", "a<b")
+        }
+    }
+}
---
+++ app/src/main/java/com/ellen/androidktoptimization/ifop/Condition.kt
@@ -0,0 +1,91 @@
+package com.ellen.androidktoptimization.ifop
+
+import android.util.Log
+
+/**
+ * 条件语句三要素:
+ * 布尔表达式 必须
+ * true回调
+ * false回调
+ */
+class Condition {
+
+    private var boolMaps: MutableMap<Int, MutableMap<Boolean, () -> Unit>?> = HashMap()
+    private var initPriority = 0
+    private var priority: Int = initPriority
+    private var endPriority = -1
+
+    fun check(condition: Boolean, trueCallback: () -> Unit, falseCallback: () -> Unit): Condition {
+        val callbacks: MutableMap<Boolean, () -> Unit> = HashMap()
+        if (condition) {
+            Log.e("Ellen2021","执行没true")
+            callbacks[true] = trueCallback
+        } else {
+            Log.e("Ellen2021","执行没false")
+            callbacks[false] = falseCallback
+        }
+        Log.e("Ellen2021","执行没check")
+        boolMaps[priority] = callbacks
+        priority++
+        return this
+    }
+
+    fun checkNoCallback(condition: Boolean): Condition {
+        if (!condition) {
+            //设置结束执行的位置
+            endPriority = priority
+        }
+        boolMaps[priority] = null
+        priority++;
+        return this
+    }
+
+    fun checkFalseCallback(condition: Boolean, falseCallback: () -> Unit): Condition {
+        val callbacks: MutableMap<Boolean, () -> Unit>?
+        if (condition) {
+            callbacks = null
+        } else {
+            callbacks = HashMap()
+            callbacks[false] = falseCallback
+        }
+        boolMaps[priority] = callbacks
+        priority++
+        return this
+    }
+
+    fun checkTrueCallback(condition: Boolean, trueCallback: () -> Unit): Condition {
+        val callbacks: MutableMap<Boolean, () -> Unit>?
+        if (condition) {
+            callbacks = HashMap()
+            callbacks[true] = trueCallback
+        } else {
+            callbacks = null
+        }
+        boolMaps[priority] = callbacks
+        priority++
+        return this
+    }
+
+    fun start() {
+        for (i in initPriority until priority) {
+            Log.e("Ellen2021","执行没start")
+            val callbacks: MutableMap<Boolean, () -> Unit>? = boolMaps[i]
+            if(i == endPriority){
+                Log.e("Ellen2021","结束位置:${endPriority}")
+                break
+            }
+            if (callbacks == null) {
+
+            } else {
+                val trueCallback: (() -> Unit)? = callbacks[true]
+                if (trueCallback != null) {
+                    trueCallback()
+                }
+                val falseCallback: (() -> Unit)? = callbacks[false]
+                if (falseCallback != null) {
+                    falseCallback()
+                }
+            }
+        }
+    }
+}
